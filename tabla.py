f48c53425127d866866e811006796d3c22d647e7
alemeoww <alexanderpoke2004@gmail.com>
2025-04-21 11:15:06 -0400

    hjkjñ

--- tabla.py
+++ tabla.py
@@ -4,19 +4,31 @@
 #for i in range(1,11):
     #print(num, "x" , i, "=", i*num)
 
-cantA=int(input("ingresa numero de alumnos"))
+#cantA=int(input("ingresa numero de alumnos"))
 
-for j in range(cantA):
-    cantN=int(input("ingrese el numero de notas del alumno", j+1))
-    suma=0
-    for i in range(cantN):
-        print("ingrese nota ", i+1) "del alumno ", j+1, ("use valores decimales")
-        nota=float(input())
-        suma+=nota
-        #suma=suma+nota
-    prom=suma/cantN
-    print("el promedio es ", prom)
-    if prom>=4:
-        print("usted aprobó")
-    else:
-        print("usted reprobó")
+#for j in range(cantA):
+#    cantN=int(input("ingrese el numero de notas del alumno", j+1))
+#    suma=0
+#    for i in range(cantN):
+#        print("ingrese nota ", i+1) "del alumno ", j+1, ("use valores decimales")
+#        nota=float(input())
+#        suma+=nota
+#        #suma=suma+nota
+#   prom=suma/cantN
+#   print("el promedio es ", prom)
+#   if prom>=4:
+#       print("usted aprobó")
+#   else:
+#       print("usted reprobó")
+
+#num=int(input("ingrese un numero "))
+
+#if num % 2==0:
+#    print("el numero",num," es par")
+#else:
+#    print("el numero",num," es impar")
+
+#designe 2 votantes. Pida la cantidad de votantes
+#pida el vodo a cada votante y muestre los resultados
+#verifique quien ganó. Considera un empate
+
