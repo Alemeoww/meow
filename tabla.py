b7fef0f793165a871f0e09eff9996e39e1a01729
alemeoww <alexanderpoke2004@gmail.com>
2025-04-21 10:35:50 -0400

    ejercicios de for

--- tabla.py
+++ tabla.py
@@ -1,5 +1,18 @@
-num=int(input("ingresa un numero: "))
+#num=int(input("ingresa un numero: "))
 
 
-for i in range(1,11):
-    print(num, "x" , i, "=", i*num)
+#for i in range(1,11):
+    #print(num, "x" , i, "=", i*num)
+
+cantA=int(input("ingresa numero de alumnos"))
+
+for j in range(cantA):
+    cantN=int(input("ingrese el numero de notas del alumno", j+1))
+    suma=0
+    for i in range(cantN):
+        print("ingrese nota ", i+1) "del alumno ", j+1, ("use valores decimales")
+        nota=float(input())
+        suma+=nota
+        #suma=suma+nota
+    prom=suma/cantN
+    print("el promedio es ", prom)
